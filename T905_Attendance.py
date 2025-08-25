e857b0f761ea4a9e0f3d031553fbdcffb9739a96
gauta25 <gautamkanwar9@gmail.com>
2025-08-25 11:04:36 -0700

    Update T905_Attendance.py

--- T905_Attendance.py
+++ T905_Attendance.py
@@ -2,22 +2,22 @@ import streamlit as st
 import pandas as pd
 from snowflake.snowpark.functions import col
 
-st.title("Website in maintainence. Will be back soon. \n-11:00am Aug 25 2025 /n - Gautam")
-st.stop()
-# --- LOGIN PAGE ---
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
+# st.title("Website in maintainence. Will be back soon. \n-11:00am Aug 25 2025 /n - Gautam")
+# st.stop()
+# # --- LOGIN PAGE ---
+# if "logged_in" not in st.session_state:
+#     st.session_state.logged_in = False
 
-if not st.session_state.logged_in:
-    st.title("Troop 905 Attendance Tracker")
-    password = st.text_input("Enter password", type="password")
-    if st.button("Login"):
-        if password == "905Member":
-            st.session_state.logged_in = True
-            st.success("Login successful! Reloading app...")
-        else:
-            st.error("Incorrect password")
-    st.stop()  # Prevents rest of app from running until login is successful
+# if not st.session_state.logged_in:
+#     st.title("Troop 905 Attendance Tracker")
+#     password = st.text_input("Enter password", type="password")
+#     if st.button("Login"):
+#         if password == "905Member":
+#             st.session_state.logged_in = True
+#             st.success("Login successful! Reloading app...")
+#         else:
+#             st.error("Incorrect password")
+#     st.stop()  # Prevents rest of app from running until login is successful
     
 # Init
 cnx = st.connection("snowflake")
