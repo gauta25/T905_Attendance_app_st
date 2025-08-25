1c5792b5b62d8bc4addcf1de73ff64a0ddd49ec2
gauta25 <gautamkanwar9@gmail.com>
2025-08-25 10:59:56 -0700

    Update T905_Attendance.py

--- T905_Attendance.py
+++ T905_Attendance.py
@@ -3,6 +3,7 @@ import pandas as pd
 from snowflake.snowpark.functions import col
 
 st.title("Website in maintainence. Will be back soon. \n-11:00am Aug 25 2025 /n - Gautam")
+st.stop()
 # --- LOGIN PAGE ---
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
