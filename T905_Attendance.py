5aa4e0c7b84b5dc538108dcd3b8498f2f390dab7
gauta25 <gautamkanwar9@gmail.com>
2025-08-25 11:15:12 -0700

    Update T905_Attendance.py

--- T905_Attendance.py
+++ T905_Attendance.py
@@ -2,6 +2,42 @@ import streamlit as st
 import pandas as pd
 from snowflake.snowpark.functions import col
 
+# ATTENDANCE PAGE FUNCTION
+def attendance():
+    cnx = st.connection("snowflake")
+    session = cnx.session()
+
+    st.title("Troop 905 Attendance Tracker")
+
+    Tdata = session.table('T905.ATTENDANCE.T905_ATTENDANCE').to_pandas()
+
+    # st.title("Troop 905 Attendance")
+
+    queried_name = st.selectbox('Choose a name to get attendance', Tdata,
+                             max_selections=1)
+
+    st.markdown("<br><br><br><br><br>", unsafe_allow_html=True)
+
+    time_to_q = st.button("Get attendance")
+
+    if time_to_q and queried_name:
+        queried_name = str(queried_name[0])
+        meetings_attended_query = (
+        f"select meetings_count from T905.ATTENDANCE.T905_ATTENDANCE "
+        f"where NAME = '{queried_name}'"
+        )
+        meetings_attended = session.sql(meetings_attended_query)
+        meetings_attended = meetings_attended.select(col('MEETINGS_COUNT')).collect()
+        meetings_attended = meetings_attended[0]['MEETINGS_COUNT']
+        meetings_possible = 6  # update automatically later if needed
+        meetings_ratio = round(meetings_attended/meetings_possible * 100, 2)
+        st.write(f"""So far, {queried_name} has attended {meetings_ratio}% 
+of meetings. \n\nThe minimum requirement for getting credit for
+a leadership position is 51%. \n\nA good amount to aim for is 90%,
+or an 'A' grade.""")
+
+    st.write("Email gautamkanwar9@gmail.com for any changes.")
+
 # --- LOGIN PAGE ---
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
@@ -13,41 +49,9 @@ if not st.session_state.logged_in:
         if password == "905Member":
             st.session_state.logged_in = True
             st.success("Login successful! Reloading app...")
+            attendance()
         else:
             st.error("Incorrect password")
     st.stop()  # Prevents rest of app from running until login is successful
     
-# Init
-cnx = st.connection("snowflake")
-session = cnx.session()
 
-st.title("Troop 905 Attendance Tracker")
-
-Tdata = session.table('T905.ATTENDANCE.T905_ATTENDANCE').to_pandas()
-
-# st.title("Troop 905 Attendance")
-
-queried_name = st.selectbox('Choose a name to get attendance', Tdata,
-                             max_selections=1)
-
-st.markdown("<br><br><br><br><br>", unsafe_allow_html=True)
-
-time_to_q = st.button("Get attendance")
-
-if time_to_q and queried_name:
-    queried_name = str(queried_name[0])
-    meetings_attended_query = (
-        f"select meetings_count from T905.ATTENDANCE.T905_ATTENDANCE "
-        f"where NAME = '{queried_name}'"
-    )
-    meetings_attended = session.sql(meetings_attended_query)
-    meetings_attended = meetings_attended.select(col('MEETINGS_COUNT')).collect()
-    meetings_attended = meetings_attended[0]['MEETINGS_COUNT']
-    meetings_possible = 6  # update automatically later if needed
-    meetings_ratio = round(meetings_attended/meetings_possible * 100, 2)
-    st.write(f"""So far, {queried_name} has attended {meetings_ratio}% 
-of meetings. \n\nThe minimum requirement for getting credit for
-a leadership position is 51%. \n\nA good amount to aim for is 90%,
-or an 'A' grade.""")
-
-st.write("Email gautamkanwar9@gmail.com for any changes.")
